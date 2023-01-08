28f14686d10247fa4611b7b339d11da0ca7e62cb
mr-deeee <merchantdaniel8@gmail.com>
2023-01-08 10:01:08 +0000

    Calender

--- lib/Views/calendarWidgets.dart
+++ lib/Views/calendarWidgets.dart
@@ -1,11 +1,11 @@
-import 'package:crib_hunter/Models/appConstants.dart';
+import 'package:antoh/Models/appConstants.dart';
 import 'package:flutter/material.dart';
 
 class CalendarMonthWidget extends StatefulWidget {
-  final int monthIndex;
-  final List<DateTime> bookedDates;
+  final int? monthIndex;
+  final List<DateTime>? bookedDates;
 
-  CalendarMonthWidget({Key key, this.monthIndex, this.bookedDates})
+  CalendarMonthWidget({Key? key, this.monthIndex, this.bookedDates})
       : super(key: key);
 
   @override
@@ -13,26 +13,26 @@ class CalendarMonthWidget extends StatefulWidget {
 }
 
 class _CalendarMonthState extends State<CalendarMonthWidget> {
-  List<MonthTile> _monthTiles;
-  int _currentMonthInt;
-  int _currentYearInt;
+  List<MonthTile> ?_monthTiles;
+  int ?_currentMonthInt;
+  int? _currentYearInt;
 
   void _setUpMonthTiles() {
     setState(() {
       _monthTiles = [];
-      int daysInMonth = AppConstants.daysInMonths[_currentMonthInt];
-      DateTime firstDayOfMonth = DateTime(_currentYearInt, _currentMonthInt, 1);
+      int? daysInMonth = AppConstants.daysInMonths[_currentMonthInt];
+      DateTime? firstDayOfMonth = DateTime(_currentYearInt!, _currentMonthInt!, 1);
       int firstWeekdayOfMonth = firstDayOfMonth.weekday;
       if (firstWeekdayOfMonth != 7) {
         for (int i = 0; i < firstWeekdayOfMonth; i++) {
-          _monthTiles.add(MonthTile(
-            dateTime: null,
+          _monthTiles?.add(MonthTile(
+            dateTime: null!,
           ));
         }
       }
-      for (int i = 1; i <= daysInMonth; i++) {
-        DateTime date = DateTime(_currentYearInt, _currentMonthInt, i);
-        _monthTiles.add(
+      for (int i = 1; i <= daysInMonth!; i++) {
+        DateTime date = DateTime(_currentYearInt!, _currentMonthInt!, i);
+        _monthTiles!.add(
           MonthTile(dateTime: date),
         );
       }
@@ -41,14 +41,14 @@ class _CalendarMonthState extends State<CalendarMonthWidget> {
 
   @override
   void initState() {
-    _currentMonthInt = (DateTime.now().month + widget.monthIndex) % 12;
+    _currentMonthInt = (DateTime.now().month + widget.monthIndex!) % 12;
     if (_currentMonthInt == 0) {
       _currentMonthInt = 12;
     }
 
     _currentYearInt = DateTime.now().year;
-    if (_currentMonthInt < DateTime.now().month) {
-      _currentYearInt += 1;
+    if (_currentMonthInt !< DateTime.now().month) {
+      //_currentYearInt! += 1;
     }
     _setUpMonthTiles();
 
@@ -66,15 +66,15 @@ class _CalendarMonthState extends State<CalendarMonthWidget> {
               "${AppConstants.monthDict[_currentMonthInt]} - $_currentYearInt"),
         ),
         GridView.builder(
-          itemCount: _monthTiles.length,
+          itemCount: _monthTiles!.length,
           shrinkWrap: true,
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 7,
             childAspectRatio: 1 / 1,
           ),
           itemBuilder: (context, index) {
-            MonthTile monthTile = _monthTiles[index];
-            if (widget.bookedDates.contains(monthTile.dateTime)) {
+            MonthTile monthTile = _monthTiles![index];
+            if (widget.bookedDates!.contains(monthTile.dateTime)) {
               return MaterialButton(
                 onPressed: null,
                 child: monthTile,
@@ -94,12 +94,12 @@ class _CalendarMonthState extends State<CalendarMonthWidget> {
 }
 
 class MonthTile extends StatelessWidget {
-  final DateTime dateTime;
+  final DateTime? dateTime;
 
-  MonthTile({this.dateTime, Key key}) : super(key: key);
+  MonthTile({this.dateTime, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(this.dateTime == null ? "" : this.dateTime.day.toString());
+    return Text(this.dateTime == null ? "" : this.dateTime!.day.toString());
   }
 }
