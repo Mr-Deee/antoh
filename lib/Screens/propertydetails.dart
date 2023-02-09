94aefeb400e369f48cb2e2cc8830447b22ddb6b5
MR.NARTERH <merchantdaniel8@gmail.com>
2023-02-09 01:08:05 +0000

    1

--- lib/Screens/propertydetails.dart
+++ lib/Screens/propertydetails.dart
@@ -31,51 +31,55 @@ appBar: AppBar(
 title: Text("PROPERTY DETAILS",style: TextStyle(color: Colors.white),),
 ),
       body: Container(
-
-        child: Expanded(
-          child: StreamBuilder(
-            stream:
-            _firestore.collection("Utiles").snapshots(),
-            builder: (
-                BuildContext context,
-                AsyncSnapshot<
-                    QuerySnapshot<Map<String, dynamic>>>
-                snapshot,
-                ) {
-              if (snapshot.hasData) {
-                final List<dynamic> _productGroups =
-                snapshot.data!.docs[0].data()['List']
-                as List<dynamic>;
-                _productGroups.sort();
-                return GridView.builder(
-                  gridDelegate:
-                  const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 2,
-                    crossAxisSpacing: 30,
-                    mainAxisSpacing: 20,
-                  ),
-                  itemCount: _productGroups.length,
-                  itemBuilder: (context, index) {
-                    return ProductGroupCard(
-                      name: _productGroups[index] as String,
-                      key: UniqueKey(),
+height: 566,
+        child: Row(
+          children: [
+            Expanded(
+              child: StreamBuilder(
+                stream:
+                _firestore.collection("Utiles").snapshots(),
+                builder: (
+                    BuildContext context,
+                    AsyncSnapshot<
+                        QuerySnapshot<Map<String, dynamic>>>
+                    snapshot,
+                    ) {
+                  if (snapshot.hasData) {
+                    final List<dynamic> _productGroups =
+                    snapshot.data!.docs[0].data()['List']
+                    as List<dynamic>;
+                    _productGroups.sort();
+                    return GridView.builder(
+                      gridDelegate:
+                      const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 2,
+                        crossAxisSpacing: 30,
+                        mainAxisSpacing: 20,
+                      ),
+                      itemCount: _productGroups.length,
+                      itemBuilder: (context, index) {
+                        return ProductGroupCard(
+                          name: _productGroups[index] as String,
+                          key: UniqueKey(),
+                        );
+                      },
                     );
-                  },
-                );
-              } else {
-                return const Center(
-                  child: SizedBox(
-                    height: 40,
-                    width: 40,
-                    child: CircularProgressIndicator(
-                      color: ColorPalette.pacificBlue,
-                    ),
-                  ),
-                );
-              }
-            },
-          ),
+                  } else {
+                    return const Center(
+                      child: SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: CircularProgressIndicator(
+                          color: ColorPalette.pacificBlue,
+                        ),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
